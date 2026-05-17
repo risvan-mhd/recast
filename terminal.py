56a3a3b7805b1cfa22ff5e45ffda10b321d30c83
Risvan Muhammad <risvan145k@gmail.com>
2026-05-17 16:50:52 +0530

    feat: add color support in rendering

--- terminal.py
+++ terminal.py
@@ -6,17 +6,66 @@ import libvterm as _vt
 
 
 @dataclass(frozen=True)
+class Color:
+    r: int
+    g: int
+    b: int
+
+    def to_tuple(self) -> tuple[int, int, int]:
+        return (
+            self.r,
+            self.g,
+            self.b,
+        )
+
+
+@dataclass(frozen=True)
 class Cell:
     row: int
     col: int
 
     char: str
+    fg: Color
+    bg: Color
 
 
 class Terminal:
     def __init__(self, rows: int, cols: int) -> None:
         self.rows = rows
         self.cols = cols
+
+        self.bg_color = rgb("#1e1e2e")
+        self.fg_color = rgb("#cdd6f4")
+        self.indexed_colors = [
+            # Catppuccin Mocha
+            rgb("#45475a"),
+            rgb("#f38ba8"),
+            rgb("#a6e3a1"),
+            rgb("#f9e2af"),
+            rgb("#89b4fa"),
+            rgb("#f5c2e7"),
+            rgb("#94e2d5"),
+            rgb("#bac2de"),
+            rgb("#585b70"),
+            rgb("#f38ba8"),
+            rgb("#a6e3a1"),
+            rgb("#f9e2af"),
+            rgb("#89b4fa"),
+            rgb("#f5c2e7"),
+            rgb("#94e2d5"),
+            rgb("#a6adc8"),
+        ]
+        levels = (0, 95, 135, 175, 215, 255)
+        self.indexed_colors.extend(
+            Color(r, g, b)
+            for r in levels
+            for g in levels
+            for b in levels  # XTerm RGB cube
+        )
+        self.indexed_colors.extend(
+            Color(level, level, level)
+            for level in range(8, 239, 10)  # Grayscale
+        )
 
         self._closed = False
 
@@ -57,6 +106,26 @@ class Terminal:
         _vt.vterm_input_write(self._vt, data, len(data))
         _vt.vterm_screen_flush_damage(self._screen)
 
+    def _color(self, c) -> Color:
+        match c.type:
+            case 0:  # RGB:
+                r = c.rgb.red
+                g = c.rgb.green
+                b = c.rgb.blue
+                return Color(r, g, b)
+
+            case 1:  # Indexed ANSI Color
+                return self.indexed_colors[c.indexed.idx]
+
+            case 2:
+                return self.fg_color
+
+            case 4:
+                return self.bg_color
+
+            case _:
+                raise ValueError(f"Unknown color type: {c.type}")
+
     def cell(self, row: int, col: int) -> Cell:
         self._ensure_open()
 
@@ -75,11 +144,16 @@ class Terminal:
             byref(raw_cell),
         )
 
+        fg = self._color(raw_cell.fg)
+        bg = self._color(raw_cell.bg)
+
         char = "".join(chr(cp) for cp in raw_cell.chars if cp != 0) or " "
         return Cell(
             row=row,
             col=col,
             char=char,
+            fg=fg,
+            bg=bg,
         )
 
     def __iter__(self) -> Generator[Cell, None, None]:
@@ -88,3 +162,8 @@ class Terminal:
         for row in range(self.rows):
             for col in range(self.cols):
                 yield self.cell(row, col)
+
+
+def rgb(hex: str) -> Color:
+    hex = hex.lstrip("#")
+    return Color(*tuple(int(hex[i : i + 2], 16) for i in (0, 2, 4)))
--- terminal_renderer.py
+++ terminal_renderer.py
@@ -12,8 +12,7 @@ class TerminalRenderer:
         self.cell_width, self.cell_height = self.cell_size()
         self.width, self.height = self.image_size()
 
-        self.bg_color = "#1e1e2e"
-        self.frame = Image.new("RGB", (self.width, self.height), self.bg_color)  # type: ignore
+        self.frame = Image.new("RGB", (self.width, self.height), self.term.bg_color.to_tuple())  # type: ignore
 
     def cell_size(self) -> tuple[int, int]:
         left, _, right, _ = self.font.getbbox("M")
@@ -34,12 +33,16 @@ class TerminalRenderer:
         frame = self.frame.copy()
         draw = ImageDraw.Draw(frame)
         for cell in self.term:
-            if cell.char == " ":
-                continue
-
             x = cell.col * self.cell_width
             y = cell.row * self.cell_height
 
-            draw.text((x, y), cell.char, "white", font=self.font)
+            if cell.bg != self.term.bg_color:
+                draw.rectangle(
+                    (x, y, x + self.cell_width, y + self.cell_height),
+                    cell.bg.to_tuple(),
+                )
+
+            if cell.char != " ":
+                draw.text((x, y), cell.char, cell.fg.to_tuple(), font=self.font)
 
         return frame
